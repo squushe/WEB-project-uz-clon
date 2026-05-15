ce2248c7faad590b708e5ce61884578cb2ee9b11
squushe bc2025-7 <maksimrozko49@gmail.com>
2026-05-15 13:10:30 +0300

    feat: configure React Router DOM with home and booking routes

--- railway-app/src/App.jsx
+++ railway-app/src/App.jsx
@@ -1,6 +1,20 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Booking from "./pages/Booking";
 
-function App() {}
+function App() {
+  return (
+    <BrowserRouter>
+      <header className="header">
+        <h2>Укрзалізниця (Клон)</h2>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking/:trainId" element={<Booking />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
