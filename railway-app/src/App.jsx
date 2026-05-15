26093f21a92fb71414e8149dc56fd095c3faf1a4
squushe bc2025-7 <maksimrozko49@gmail.com>
2026-05-15 13:12:23 +0300

    feat: add react-toastify notifications on successful booking

--- railway-app/src/App.jsx
+++ railway-app/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Home from "./pages/Home";
 import Booking from "./pages/Booking";
 
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/booking/:trainId" element={<Booking />} />
       </Routes>
+
+      <ToastContainer position="bottom-right" autoClose={3000} />
     </BrowserRouter>
   );
 }
